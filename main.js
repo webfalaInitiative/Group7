c8be043b3cf07537bd3b271b059687ab5943fd2a
sweathannah <sultanatmbashir@gmail.com>
2024-03-13 13:17:04 +0100

    styles

--- main.js
+++ main.js
@@ -2,14 +2,14 @@
 
     document.addEventListener("DOMContentLoaded", function () {
         // Select the "Contact" link
-        var contactLink = document.getElementById("contact");
+        let contactLink = document.getElementById("contact");
     
         // Add click event listener to the "Contact" link
         contactLink.addEventListener("click", function (event) {
             event.preventDefault(); // Prevent the default link behavior
     
             // Select the nested ul
-            var nestedUl = contactLink.nextElementSibling;
+            let nestedUl = contactLink.nextElementSibling;
     
             // Toggle the visibility of the nested ul
             nestedUl.classList.toggle('hidden');
@@ -17,11 +17,31 @@
     });
 
     document.addEventListener("DOMContentLoaded", function () {
-        var closeDropdownButton = document.getElementById("closeDropdown");
-        var dropdown = document.getElementById("dropdown");
+        let closeDropdownButton = document.getElementById("closeDropdown");
+        let dropdown = document.getElementById("dropdown");
     
         closeDropdownButton.addEventListener("click", function () {
             dropdown.classList.add("hidden");
         });
     });
+
+    document.addEventListener("DOMContentLoaded", function () {
+       let menulink = document.getElementById("menu");
+
+       menulink.addEventListener("click", function (event) {
+            event.preventDefault();
+            let nestedLi = menulink.nextElementSibling;
+                nestedLi.classList.toggle('hidden')
+       })
+    })
     
+    document.addEventListener("DOMContentLoaded", function () {
+        let menuClose = document.getElementById("menu-close");
+        let menudrop = document.getElementById("menudrop");
+
+        menuClose.addEventListener("click",function 
+        () {
+            menudrop.classList.add("hidden")
+        })
+    })
+
